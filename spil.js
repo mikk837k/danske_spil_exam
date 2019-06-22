a061de96097a4ea3323376c833808d1e3b248bad
Mikkel Madsen <mikkelmadsen@mikkels-air.home>
2019-06-22 10:06:57 +0200

    school of fish code is moved to spil.js

--- spil.js
+++ spil.js
@@ -1,6 +1,13 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", init);
+// window.addEventListener("DOMContentLoaded", () => {
+//   const head = document.querySelector("head");
+//   const delayedStyle = document.createElement("link");
+//   delayedStyle.setAttribute("rel", "stylesheet");
+//   delayedStyle.setAttribute("href", `/spil`);
+//   head.appendChild(delayedStyle);
+// });
 
 import myJsonImport from "/static/garbage.json";
 import is from "is_js";
@@ -15,6 +22,8 @@ const gameContainer = document.querySelector("#game_container");
 const container = document.querySelector(".container");
 const startknap_mobil = document.querySelector(".mobil_start");
 const genstart_knap = document.querySelector(".genstart");
+const schoolOfFish = document.querySelector("#school_of_fish");
+let elementIsToRight = true;
 
 function init() {
   // Eventlistener som lytter på alle mousedown events på body
@@ -43,7 +52,6 @@ function buildGame() {
     gameContainer.appendChild(newDiv);
   });
   placeElements();
-  randomStartPos();
 }
 
 function windowClicked(e) {
@@ -58,6 +66,7 @@ function windowClicked(e) {
   if (action === "start") {
     buildGame();
     showRules();
+    randomStartPos();
   }
 
   if (!isGameWon && action === "mobil_start") {
@@ -346,3 +355,93 @@ const activateElement = function(myElement) {
   myElement.style.opacity = "1";
   myElement.style.pointerEvents = "all";
 };
+
+//SCHOOL OF FISH ANIMATION
+
+function randomStartPos() {
+  //console.log("randomStartPos");
+  let yPos = calculateYPos(gameContainer, schoolOfFish).toString();
+  // let xPos = calculateEndPosRight(gameContainer, schoolOfFish).toString();
+  let xPos = gameContainer.clientWidth;
+
+  schoolOfFish.style.transform = `translate(${xPos}px, ${yPos}px)`;
+
+  // //console.log(containerXpos, yPos, xPos);
+
+  chooseDirection(yPos, xPos);
+}
+
+function randomYPos(yPos, xPos) {
+  // //console.log("randomYpos", xPos);
+  let newYpos = calculateYPos(gameContainer, schoolOfFish).toString();
+
+  schoolOfFish.style.transform = `translate(${xPos}px, ${newYpos}px)`;
+
+  chooseDirection(newYpos, xPos);
+}
+
+function chooseDirection(yPos, xPos) {
+  //   //console.log("generateNewYPos", yPos);
+
+  schoolOfFish.style.transitionDuration = "0";
+
+  if (elementIsToRight === true) {
+    // //console.log(yPos);
+    schoolOfFish.style.transitionDuration = "200ms";
+    schoolOfFish.style.transform = `translate(${xPos}px, ${yPos}px) scaleX(1)`;
+    setTimeout(() => {
+      animateLeft(yPos);
+    }, 1000);
+  } else {
+    schoolOfFish.style.transitionDuration = "200ms";
+    schoolOfFish.style.transform = `translate(${xPos}px, ${yPos}px) scaleX(-1)`;
+    setTimeout(() => {
+      animateRight(yPos);
+    }, 1000);
+  }
+}
+
+// Animate school of fish
+
+function animateLeft(yPos) {
+  // let xPos = calculateEndPosLeft(gameContainer, schoolOfFish).toString();
+  let xPos = calculateEndPosLeft(schoolOfFish);
+  // //console.log("animateLeft", xPos);
+
+  schoolOfFish.style.transform = `translate(${xPos}px, ${yPos}px) scaleX(1)`;
+  schoolOfFish.style.transitionDuration = "5s";
+  schoolOfFish.style.transitionTimingFunction = "linear";
+
+  elementIsToRight = false;
+
+  setTimeout(() => {
+    randomYPos(yPos, xPos);
+  }, 10000);
+}
+
+function animateRight(yPos) {
+  // let xPos = calculateEndPosRight(gameContainer, schoolOfFish).toString();
+  let xPos = gameContainer.clientWidth;
+
+  //   //console.log("animateRight", xPos, yPos);
+  schoolOfFish.style.transitionDuration = "5s";
+  schoolOfFish.style.transitionTimingFunction = "linear";
+  schoolOfFish.style.transform = `translate(${xPos}px, ${yPos}px) scaleX(-1)`;
+
+  elementIsToRight = true;
+
+  setTimeout(() => {
+    randomYPos(yPos, xPos);
+  }, 10000);
+}
+
+// Position calculations
+
+function calculateYPos(container, elem) {
+  return Math.floor(
+    Math.random() * (container.clientHeight - elem.clientHeight)
+  );
+}
+function calculateEndPosLeft(elem) {
+  return 0 - elem.clientWidth;
+}
